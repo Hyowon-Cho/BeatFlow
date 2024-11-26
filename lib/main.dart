3f77be9062cd6e441cf7a098b4a1cdb8a5714e9d
Hyowon-Cho <baltimorecho@gmail.com>
2024-11-26 18:11:20 -0500

    Func: Add Recording player and Revised pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BeatFlow',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FlutterSoundRecorder? _audioRecorder;
+  FlutterSoundPlayer? _audioPlayer;
+  bool _isRecording = false;
+  bool _isPlaying = false;
+  String _audioFilePath = 'audio_file.aac';
+  bool _isSupported = !kIsWeb && !Platform.isWindows;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    if (_isSupported) {
+      _audioRecorder = FlutterSoundRecorder();
+      _audioPlayer = FlutterSoundPlayer();
+      initAudio();
+    }
+  }
+
+  Future<void> initAudio() async {
+    if (_isSupported) {
+      await _audioRecorder!.openRecorder();
+      await _audioPlayer!.openPlayer();
+    }
+  }
+
+  @override
+  void dispose() {
+    if (_isSupported) {
+      _audioRecorder!.closeRecorder();
+      _audioPlayer!.closePlayer();
+    }
+    super.dispose();
+  }
+
+  Future<void> startRecording() async {
+    if (_isSupported) {
+      await _audioRecorder!.startRecorder(toFile: _audioFilePath);
+      setState(() {
+        _isRecording = true;
+      });
+    }
+  }
+
+  Future<void> stopRecording() async {
+    if (_isSupported) {
+      await _audioRecorder!.stopRecorder();
+      setState(() {
+        _isRecording = false;
+      });
+    }
+  }
+
+  Future<void> startPlaying() async {
+    if (_isSupported) {
+      await _audioPlayer!.startPlayer(fromURI: _audioFilePath);
+      setState(() {
+        _isPlaying = true;
+      });
+    }
+  }
+
+  Future<void> stopPlaying() async {
+    if (_isSupported) {
+      await _audioPlayer!.stopPlayer();
+      setState(() {
+        _isPlaying = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('BeatFlow Audio Recorder & Player'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            if (_isSupported) ...[
+              ElevatedButton(
+                onPressed: _isRecording ? stopRecording : startRecording,
+                child: Text(_isRecording ? 'Stop Recording' : 'Start Recording'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _isPlaying ? stopPlaying : startPlaying,
+                child: Text(_isPlaying ? 'Stop Playing' : 'Start Playing'),
+              ),
+            ] else
+              const Text('Audio features are not supported on this platform'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
